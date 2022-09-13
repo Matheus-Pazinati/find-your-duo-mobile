55d201adb5a66559f1862b6b3d63d2e89cafe212
Matheus Pazinati <matheuspazinati@gmail.com>
2022-09-13 16:19:23 -0300

    Altera imagem de splash, baixa libs necessarias, e cria componente de background da aplicacao

--- App.tsx
+++ App.tsx
@@ -1,36 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-
-type ButtonProps = {
-  text: string;
-}
-
-function Button({ text }: ButtonProps) {
-  return (
-    <TouchableOpacity>
-      <Text>
-        {text}
-      </Text>
-    </TouchableOpacity>
-  )
-}
+import { Background } from './src/components/Background';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello React-Native</Text>
-      <StatusBar style="auto" />
+    <Background>
 
-      <Button text="Send" />
-    </View>
+    </Background>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
